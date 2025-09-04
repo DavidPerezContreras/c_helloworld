1409e79df24f76bb0bfd10fa3befe488ae1122c1
John Rambo <john_rambo69@gmail.com>
2025-09-04 23:22:32 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+
+
+int main(int argc, char **argv)
+{
+    char buffer[5];
+    for (int i = 0; i <= 5; i++) { // Off-by-one error: writes past buffer[4]
+        buffer[i] = 'A' + i;
+    }
+
+    buffer[4] = '\0';
+    printf("Buffer: %s\n", buffer);
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 📐  C Hello World
+
+A C hello world that causes a buffer overflow to test the runtime checks
+
+---
